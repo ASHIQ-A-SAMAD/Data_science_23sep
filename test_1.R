fac35a9a09f1b528adf3aeff7c0657ca1d9ccbdb
ASHIQ-A-SAMAD <71721760+ASHIQ-A-SAMAD@users.noreply.github.com>
2020-09-22 22:16:43 +0530

    Add files via upload
    r tutorial

---
+++ test_1.R
@@ -0,0 +1,75 @@
+n<-c(1,2,4.5)
+o<-c(5,6,8)
+n+o
+n*o
+n%%0
+n%%o
+n<-10
+n<-c(10,100,1000)
+x<-10
+n+x
+n/x
+n/0
+n%%0
+#relational
+q<-c(1,2,3)
+r<-c(2,1,3)
+q>r
+q>=r
+q[1]>r[2
+      ]
+q[1]==q[3]
+       q[1]!=q[3]
+       
+       
+       t=c(1,2)
+t[1]=5
+
+
+
+
+
+c(1,4,5,6)->a
+a=3
+b=4
+a&b
+a&&b
+
+
+
+#-----madam-----#
+q <- c(1, 3, 4, 3, 2) 
+r <- c(2, 4, 4, 5, 2)
+# 1. > greater than 
+q>r
+q[1] > r[2]
+1 > 4
+# 2. < lesser than
+q < r
+
+# 3. == equal to Comparison operator
+q == r
+# 4. >= greater than or equal to
+q >= r
+
+# 5. <= lesser than or equal to 
+q <= r
+
+# 6. != not equal to 
+q != r
+2 != 3 
+
+
+# Assignment Operators
+# 1. Left Assignment <- or == or <<-
+v1 <- c(3,1,TRUE)
+v2 <<- c(3,1,TRUE)
+v3 = c(3,1,FALSE)
+v1
+v2
+v3
+# 2. Right Assignment -> or ->>
+c(3,1,TRUE,2+3i) -> v4
+c(3,1,TRUE,2+3i) ->> v5
+v4
+v<-3
